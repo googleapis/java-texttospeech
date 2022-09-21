ca68f3ffe7fe15125808679930cc5342b060c4a0
Mridula Peddada <mpeddada@google.com>
2022-09-21 00:30:48 -0400

    exclude from owlbot

--- owlbot.py
+++ owlbot.py
@@ -25,6 +25,7 @@ java.common_templates(
     excludes=[
         ".kokoro/release/publish_javadoc.sh",
         ".kokoro/release/publish_javadoc11.sh",
-        ".kokoro/release/stage.sh"
+        ".kokoro/release/stage.sh",
+        "renovate.json"
     ]
 )
